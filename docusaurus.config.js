eeccd44d89795743e1cc6b666845d8d3d16af214
21km43 <21km43@gmail.com>
2023-01-09 19:09:25 +0900

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,10 +94,6 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
                 label: 'Discord',
                 href: 'https://discordapp.com/invite/docusaurus',
               },
