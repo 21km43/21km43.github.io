a2e51e63e98296d465ea83b800530fc751f0ec50
21km43 <21km43@gmail.com>
2023-01-19 13:53:42 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/index">
-            Docusaurus Tutorial
+            to="/docs">
+            Documents
           </Link>
         </div>
       </div>
